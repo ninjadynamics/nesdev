864bac9f3218e2b45e1d5fd905c286d64866eeee
ninjadynamics <brunosilva.work@gmail.com>
2021-09-14 12:19:33 +0200

    Add save/load state functions

--- ninjapad.js
+++ ninjapad.js
@@ -317,3 +317,11 @@ function loadNinjaPad(gameScreen) {
         }
     );
 }
+
+function loadState(s) {
+    nes.fromJSON(JSON.parse(s));
+}
+
+function saveState() {
+    return JSON.stringify(nes.toJSON());
+}
