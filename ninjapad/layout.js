bb6d57630dd9445270a05f50f1526c3160fe5512
ninjadynamics <brunosilva.work@gmail.com>
2021-09-21 12:16:55 +0200

    New layout (wip)

--- ninjapad/layout.js
+++ ninjapad/layout.js
@@ -9,40 +9,57 @@ function setOSDLayout() {
     jQElement.osd.append(pauseScreen.content);
 }
 
-function setDesktopLayout() {
-    jQElement.screen.height("100%");
-    let newHeight = jQElement.screen.height();
-    jQElement.screen.width(256 * (newHeight / 240));
+function setDesktopLayout(width, height) {
+    DEBUG && console.log("Desktop mode");
+    if (width > height) {
+        jQElement.screen.height("100%");
+        let newHeight = jQElement.screen.height();
+        jQElement.screen.width(256 * (newHeight / 240));
+    }
+    else {
+        jQElement.screen.width("100%");
+        let newWidth = jQElement.screen.width();
+        jQElement.screen.height(240 * (newWidth / 256));
+    }
     jQElement.ninjaPad.height("0%");
     jQElement.controller.hide();
 }
 
-function setMobileLayout(height) {
-    let opacity = 1;
-    let bottom = "auto";
+function setMobileLayout(width, height) {
+    DEBUG && console.log("Mobile mode");
+    if (height >= width || window.matchMedia("(orientation: portrait)").matches) {
+        let opacity = 1;
+        let bottom = "auto";
 
-    jQElement.screen.width("100%");
-    let newWidth = jQElement.screen.width();
-    jQElement.screen.height(240 * (newWidth / 256));
+        jQElement.screen.width("100%");
+        let newWidth = jQElement.screen.width();
+        jQElement.screen.height(240 * (newWidth / 256));
 
-    let padHeight = vw(47.5);
-    let remainingHeight = height - jQElement.screen.height();
-    jQElement.ninjaPad.height(Math.max(padHeight, remainingHeight));
+        let padHeight = vw(47.5);
+        let remainingHeight = height - jQElement.screen.height();
+        jQElement.ninjaPad.height(Math.max(padHeight, remainingHeight));
 
-    let difference = remainingHeight - padHeight;
-    if (difference < 0) {
-        opacity += (difference / (padHeight * 2));
-        bottom = 0;
+        let difference = remainingHeight - padHeight;
+        if (difference < 0) {
+            opacity += (difference / (padHeight * 2));
+            bottom = 0;
+        }
+        jQElement.ninjaPad.css("bottom", bottom);
+        jQElement.ninjaPad.css("display", "block");
+
+        jQElement.controller.css("opacity", opacity);
+        jQElement.controller.show();
+
+        if (cannotResume) {
+            cannotResume = false;
+            pauseEmulation();
+        }
+        DEBUG && console.log("Show touch controls");
     }
-    jQElement.ninjaPad.css("bottom", bottom);
-    jQElement.ninjaPad.css("display", "block");
-
-    jQElement.controller.css("opacity", opacity);
-    jQElement.controller.show();
-
-    if (cannotResume) {
-        cannotResume = false;
-        pauseEmulation();
+    else {
+        setDesktopLayout(width, height);
+        handleLandscapeMode();
+        DEBUG && console.log("Hide touch controls");
     }
 }
 
@@ -50,15 +67,7 @@ function setPageLayout() {
     let useJQuery = !isFullScreen() || isIOSDevice();
     let w = useJQuery ? $(window).width() : window.innerWidth; // window.screen.availWidth;
     let h = useJQuery ? $(window).height() : window.innerHeight; // window.screen.availHeight;
-    if (h >= w || window.matchMedia("(orientation: portrait)").matches) {
-        DEBUG && console.log("Show touch controls");
-        setMobileLayout(h);
-    }
-    else {
-        setDesktopLayout();
-        if (isMobileDevice()) handleLandscapeMode();
-        DEBUG && console.log("Hide touch controls");
-    }
+    isMobileDevice() ? setMobileLayout(w, h) : setDesktopLayout(w, h);
     setOSDLayout();
 }
 
